562cb6365d6123f719852b74fa0f769a6af81f00
Dio12334 <diego.paredes.r@utec.edu.pe>
2022-11-27 19:46:58 -0500

    Display updated

--- Display/display.cpp
+++ Display/display.cpp
@@ -1,5 +1,8 @@
 #include "display.h"
+#include <SDL2/SDL_render.h>
 #include <iostream>
+
+#define WINDOWSIZE 600
 
 Display::Display(std::vector <Point> points): m_Points(points){}
 
@@ -9,7 +12,7 @@ bool Display::Init(){
         SDL_Log("\n[ERR] No se pudo inicializar SDL: %s", SDL_GetError());
         return false;
     }
-    m_Window = SDL_CreateWindow("TSP", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 600,0);
+    m_Window = SDL_CreateWindow("TSP", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOWSIZE, WINDOWSIZE,0);
     if(!m_Window){
         SDL_Log("\n[ERR] Falla en la creación de la ventana: %s", SDL_GetError());
         return false;
@@ -53,15 +56,57 @@ void Display::GenerateOutput(){
     SDL_SetRenderDrawColor(m_Renderer, 255, 255, 255, 255);
     SDL_RenderClear(m_Renderer);
 
-    int offset = 4;
-    int scale = 5;
+	auto puntos = m_Points;
+
+	unsigned int minX, maxX, minY, maxY;
+	for(size_t i = 0; i < puntos.size(); ++i){
+		if(i == 0){
+			minX = maxX = puntos[i].x;
+			minY = maxY = puntos[i].y;
+		}
+		if(puntos[i].x < minX) minX = puntos[i].x; 
+		if(puntos[i].y < minY) minY = puntos[i].y; 
+		if(puntos[i].x > maxX) maxX = puntos[i].x; 
+		if(puntos[i].y > maxY) maxY = puntos[i].y; 
+	}
+	
+	int sizeX = maxX - minX, sizeY = maxY - minY;
+
+	for(size_t i = 0; i < puntos.size(); ++i){
+		puntos[i].x *= WINDOWSIZE/sizeX - 10;
+		puntos[i].y *= WINDOWSIZE/sizeY - 10;
+	}
+
+
+	for(size_t i = 0; i < puntos.size(); ++i){
+		if(i == 0){
+			minX = maxX = puntos[i].x;
+			minY = maxY = puntos[i].y;
+		}
+		if(puntos[i].x < minX) minX = puntos[i].x; 
+		if(puntos[i].y < minY) minY = puntos[i].y; 
+		if(puntos[i].x > maxX) maxX = puntos[i].x; 
+		if(puntos[i].y > maxY) maxY = puntos[i].y; 
+	}
+	
+	sizeX = maxX - minX; sizeY = maxY - minY;
+
+    int offsetX = (WINDOWSIZE - sizeX)/2, offsetY = (WINDOWSIZE - sizeY)/2;
+    int scale = 1;
     SDL_SetRenderDrawColor(m_Renderer, 255, 0, 0, 255);
-    for(auto point: m_Points)
-        SDL_RenderDrawPoint(m_Renderer, scale*point.x + offset, scale*point.y + offset);
-    
+    for(auto point: puntos){
+		SDL_Rect rec{
+			point.x + offsetX - 3,
+			point.y + offsetY - 3,
+			6,
+			6
+		};
+		SDL_RenderFillRect(m_Renderer, &rec);
+	}
+	
     SDL_SetRenderDrawColor(m_Renderer, 0, 0, 0, 255);
-    for(int i = 0; i < m_Points.size() - 1; ++i){
-        SDL_RenderDrawLine(m_Renderer, scale*m_Points[i].x + offset, scale*m_Points[i].y + offset, scale*m_Points[i + 1].x + offset, scale*m_Points[i + 1].y + offset);
+    for(int i = 0; i < puntos.size() - 1; ++i){
+        SDL_RenderDrawLine(m_Renderer, scale*puntos[i].x + offsetX, scale*puntos[i].y + offsetY, scale*puntos[i + 1].x + offsetX, scale*puntos[i + 1].y + offsetY);
     }
 
     SDL_RenderPresent(m_Renderer);
